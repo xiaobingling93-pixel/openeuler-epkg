5d465d40a2df77e3fdd54a0468dc84bbfab505d9
small_leek <xiasenlin1@huawei.com>
2024-07-05 14:34:44 +0800

    add epkg/bin to PATH

--- lib/epkg/epkg-rc.sh
+++ lib/epkg/epkg-rc.sh
@@ -46,7 +46,7 @@ __epkg_update_path() {
 		__epkg_add_path $EPKG_ENV_NAME
 	fi
 
-	path=$path:$HOME/epkg_manager/bin
+	path=$HOME/epkg_manager/bin:$path
 	__epkg_create_path_rc "$path"
 	__epkg_rehash
 }
