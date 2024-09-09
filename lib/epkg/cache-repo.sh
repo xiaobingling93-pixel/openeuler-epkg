63917357c27bbaec5ec559ce8241dce9c3830a06
Luan Shengde <shdluan@163.com>
2024-09-09 15:01:09 +0800

    optimize cache repo
    Signed-off-by: Luan Shengde <shdluan@163.com>

--- lib/epkg/cache-repo.sh
+++ lib/epkg/cache-repo.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
 
-CHANNEL_CONF_PATH=/etc/epkg-confs
-
 cache_repo_index()
 {
-	local repo_url=$1
+	local repo_name=$1
+	local repo_url=$2
 	local local_cache_path=$HOME/.cache/epkg/channel/${repo_url##*/channel/}
+
+	echo "Caching repodata for: $repo_name"
 
 	# clean old metadata files and re-init metadata dir
 	rm -rf ${local_cache_path} && \
@@ -15,17 +16,17 @@ cache_repo_index()
 	# sync repo metadata from local path
 	[[ $repo_url =~ ^/ ]] && {
 		cp -r $repo_url/repodata/store-paths.zst ${local_cache_path}/repodata/
-		cp -r $repo_url/repodata/pkg-info.zst ${local_cache_path}/
-		cp -r $repo_url/repodata/index.yaml ${local_cache_path}/
+		cp -r $repo_url/repodata/pkg-info.zst ${local_cache_path}/repodata/
+		cp -r $repo_url/repodata/index.yaml ${local_cache_path}/repodata/
 	}
 
 	# sync repo metadata from http urls
 	[[ $repo_url =~ ^http ]] && {
 		wget --tries=3 -q $repo_url/repodata/store-paths.zst -O ${local_cache_path}/repodata/store-paths.zst.tmp && \
-		mv ${local_cache_path}/repodata/store-paths.zst.tmp ${local_cache_path}/store-paths/initial-release.zst
+		mv ${local_cache_path}/repodata/store-paths.zst.tmp ${local_cache_path}/repodata/store-paths.zst
 
 		wget --tries=3 -q $repo_url/repodata/pkg-info.zst -O ${local_cache_path}/repodata/pkg-info.zst.tmp && \
-		mv ${local_cache_path}/repodata/pkg-info.tar.zst.tmp ${local_cache_path}/repodata/pkg-info.zst
+		mv ${local_cache_path}/repodata/pkg-info.zst.tmp ${local_cache_path}/repodata/pkg-info.zst
 
 		wget --tries=3 -q $repo_url/repodata/index.yaml -O ${local_cache_path}/repodata/index.yaml.tmp && \
 		mv ${local_cache_path}/repodata/index.yaml.tmp ${local_cache_path}/repodata/index.yaml
@@ -43,8 +44,10 @@ cache_repo_index()
 	}
 
 	# cached medatata file should be decompressed
-	zstd -d ${local_cache_path}/repodata/store-paths.zst
+	zstd -d -q ${local_cache_path}/repodata/store-paths.zst
 	tar --use-compress-program=zstd -xf ${local_cache_path}/repodata/pkg-info.zst -C ${local_cache_path}/
+
+	echo "Cache repodata succeed: $repo_name"
 }
 
 loop_cache_repos()
@@ -52,12 +55,19 @@ loop_cache_repos()
 	local channel_conf_file=$1
 	local channel_conf_content=$(cat $channel_conf_file)
 
+	local channel_content
+	local repo_name
+	local repo_enable_code
+	local repo_url
+
 	for i in $(echo ${channel_conf_content} | jq '.channel | keys[]')
 	do
-		channel_content=$(echo "${channel_conf_content}" | jq '.channel["'$i'"]')
+		# channel_content=$(echo "${channel_conf_content}" | jq '.channel['"$i"']')
+		channel_content=$(echo "${channel_conf_content}" | jq '.channel['"$i"']')
 
 		[[ ${channel_content} == null ]] && break
 
+		repo_name=$(echo ${channel_content} | jq '.name' | tr -d '"')
 		repo_enable_code=$(echo ${channel_content} | jq '.enabled' | tr -d '"')
 
 		# skip cache metadata for disabled repos
@@ -67,16 +77,16 @@ loop_cache_repos()
 
 		[[ -z ${repo_url} ]] && continue
 
-		cache_repo_index $repo_url
+		cache_repo_index $repo_name $repo_url
 	done
 }
 
-main()
+cache_repo()
 {
-	for repo_conf_file in $(find $CHANNEL_CONF_PATH} -name *.json)
+	CHANNEL_CONF_PATH=${CHANNEL_CONF_PATH:-/etc/epkg-confs}
+
+	for repo_conf_file in $(find ${CHANNEL_CONF_PATH} -name *.json)
 	do
 		loop_cache_repos ${repo_conf_file}
 	done
 }
-
-main
