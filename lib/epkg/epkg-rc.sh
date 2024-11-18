25f8fb279c1b09ec90bda950b28693d30cee949b
Yingjiahui <ying_register@163.com>
2024-11-18 19:53:54 +0800

    improve __epkg_add_appbin_path

--- lib/epkg/epkg-rc.sh
+++ lib/epkg/epkg-rc.sh
@@ -6,28 +6,28 @@
 
 __epkg_add_appbin_path() {
 	local PATH_ARRAY
-	local NEW_PATH
+	local SYSTEM_ORIGIN_PATH
 
 	# Use IFS (Internal Field Separator) to split the PATH into an array
 	IFS=':' read -ra PATH_ARRAY <<< "$PATH"
 
 	# Create a new PATH variable without the unwanted directories
-	NEW_PATH=""
+	SYSTEM_ORIGIN_PATH=""
 	for dir in "${PATH_ARRAY[@]}"; do
-		if [[ "${dir#*/app-bin}" = "$dir" ]]; then
+		if [[ -n "$dir" && "${dir#*/app-bin}" = "$dir" ]]; then
 			# Append the directory to the new PATH if it doesn't end with /app-bin
-			NEW_PATH+="$dir:"
+			SYSTEM_ORIGIN_PATH+="$dir:"
 		fi
 	done
 
 	# Remove the trailing colon
-	NEW_PATH=${NEW_PATH%:}
+	SYSTEM_ORIGIN_PATH=${SYSTEM_ORIGIN_PATH%:}
 
 	# Export the new PATH
 	local HOME_EPKG=$HOME/.epkg
 	local EPKG_CONFIG_DIR=$HOME_EPKG/config
 	source $EPKG_CONFIG_DIR/shell-cmd-path.sh
-	export PATH="$EPKG_APPBIN_PATH:$NEW_PATH"
+	export PATH="$EPKG_APPBIN_PATH:$SYSTEM_ORIGIN_PATH"
 	[ -n "$epkg_active_env_path" ] && export PATH="$epkg_active_env_path:$PATH"
 
 	if [ -n "${ZSH_VERSION}" ]; then
@@ -39,7 +39,7 @@ __epkg_add_appbin_path() {
 
 epkg() {
 	local cmd="$1"
-
+	echo "$$"
 	if [ -d "/opt/epkg/users/public/envs/common/" ]; then
 		local project_dir=/opt/epkg/users/public/envs/common/profile-1/usr
 	elif [ -d "$COMMON_PROFILE_LINK" ]; then
