b032dc3fb5e0d197d6f0ab5485d4ef82c302dd29
Yingjiahui <ying_register@163.com>
2024-11-29 09:22:37 +0800

    add pkgvars example

---
+++ build/test/pkgvars-example.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+name="redis"
+version="4.0.14"
+release="7"
+license="BSD-3-Clause and MIT"
+homepage="https://redis.io"
+sources=("https://gitee.com/src-openeuler/redis/raw/master/redis-4.0.14.tar.gz" "https://gitee.com/rkingkoyo/epkg_test/releases/download/redis-4.0.14/redis.logrotate" "https://gitee.com/rkingkoyo/epkg_test/releases/download/redis-4.0.14/redis-sentinel.service" "https://gitee.com/rkingkoyo/epkg_test/releases/download/redis-4.0.14/redis.service")
+patches=("https://gitee.com/rkingkoyo/epkg_test/releases/download/redis-4.0.14/CVE-2020-14147.patch" "https://gitee.com/rkingkoyo/epkg_test/releases/download/redis-4.0.14/improved-HyperLogLog-cardinality-estimation.patch" "https://gitee.com/rkingkoyo/epkg_test/releases/download/redis-4.0.14/Aesthetic-changes-to-PR.patch")
+buildSystem="make"
+buildRequires="make gcc gawk systemd logrotate shadow-utils"
+prepPhase="     sed -i -e 's|^logfile .*$|logfile /var/log/redis/redis.log|g' redis.conf
+        sed -i -e '$ alogfile /var/log/redis/sentinel.log' sentinel.conf
+        sed -i -e 's|^dir .*$|dir /var/lib/redis|g' redis.conf"
