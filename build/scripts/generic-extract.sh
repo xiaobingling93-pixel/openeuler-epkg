ec49203a0efd9a90f63d373b182ecfe9246a5629
Yingjiahui <ying_register@163.com>
2024-11-30 03:10:57 +0800

    # Todo: Checksum Check

--- build/scripts/generic-extract.sh
+++ build/scripts/generic-extract.sh
@@ -2,12 +2,10 @@
 # SPDX-License-Identifier: MulanPSL-2.0+
 # Copyright (c) 2024 Huawei Technologies Co., Ltd. All rights reserved.
 
+# Todo: Checksum Check
 pkg_decompress() {
-    # 遍历目录中的所有文件
     find "$BUILD_SOURCES_DIR" -type f | while read -r file; do
-        # 获取文件扩展名
         ext="${file##*.}"
-        # 根据文件扩展名解压
         case "$ext" in
             zip)
                 unzip -q "$file" -d "$BUILD_SRC_DIR"
