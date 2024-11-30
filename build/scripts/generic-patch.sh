5a9c27a668b9b8a43db4786997a9d3cb5a529c4a
Yingjiahui <ying_register@163.com>
2024-11-30 20:23:04 +0800

    add generi_patch.sh

---
+++ build/scripts/generic-patch.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: MulanPSL-2.0+
+# Copyright (c) 2024 Huawei Technologies Co., Ltd. All rights reserved.
+
+pkg_patch() {
+    # 遍历patches文件夹，直接打patch
+}
