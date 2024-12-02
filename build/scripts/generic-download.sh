3abf6590d0dcbaf8aac7af6986c9bec05da42d04
Yingjiahui <ying_register@163.com>
2024-12-02 14:38:00 +0800

    improve coding style

--- build/scripts/generic-download.sh
+++ build/scripts/generic-download.sh
@@ -25,18 +25,14 @@ src_unpack() {
     case "$ext" in
         zip)
             unzip -q "$file" -d "$BUILD_SRC_DIR"
-            echo "Decompress success: $file"
             ;;
         tar.gz|tgz|gz)
             tar -xzf "$file" -C "$BUILD_SRC_DIR"
-            echo "Decompress success: $file"
             ;;
         tar.bz2)
             tar -xjf "$file" -C "$BUILD_SRC_DIR"
-            echo "Decompress success: $file"
             ;;
         *)
-            echo "Unknown format or no need to decompress: $file"
             ;;
     esac
 }
