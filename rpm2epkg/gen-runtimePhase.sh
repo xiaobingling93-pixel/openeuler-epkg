4cc2b6cb8e122bcadf0115133180666b0635095c
Yingjiahui <ying_register@163.com>
2024-12-23 17:22:17 +0800

    improve gen-runtimePhase.sh: Modify the code based on the review comments.

--- rpm2epkg/gen-runtimePhase.sh
+++ rpm2epkg/gen-runtimePhase.sh
@@ -5,43 +5,66 @@
 rpm_package="$1"
 output_directory="$2/runtimePhase"
 mkdir -p "$output_directory"
+scripts_record=()
 
-get_phase_content() {
-    local scripts_content=$(rpm -qp --nosignature --scripts "$rpm_package")
+generate_to_file() {
+    local cur_scripts=$1
+    local content=$2
+    if [[ -n "$cur_scripts" ]]; then
+        if [[ -z "${script_files[$cur_scripts]}" ]]; then
+            echo "Error: Invalid script type '$cur_scripts'. Aborting." >&2
+            exit 1
+        fi
+        echo "$content" > "$output_directory/${script_files[$cur_scripts]}"
+        chmod +x "$output_directory/${script_files[$cur_scripts]}"
+    fi
+}
 
-    declare -A script_files=(
-        ["pre"]="pre.sh"
-        ["pretrans"]="pretrans.sh"
-        ["preinstall"]="preinstall.sh"
-        ["preupgrade"]="preupgrade.sh"
-        ["preuninstall"]="preuninstall.sh"
-        ["post"]="post.sh"
-        ["posttrans"]="posttrans.sh"
-        ["postinstall"]="postinstall.sh"
-        ["postupgrade"]="postupgrade.sh"
-        ["postuninstall"]="postuninstall.sh"
-    )
-
+extract_install_scripts() {
     local current_script=""
     local script_content=""
+    
+    # https://github.com/rpm-software-management/rpm/blob/master/tests/rpmbuild.at
+    declare -A script_files=(
+        ["pretrans"]="pretrans"
+        ["preinstall"]="pre"
+        ["postinstall"]="post"
+        ["preuninstall"]="preun"
+        ["postuninstall"]="postun"
+        ["posttrans"]="posttrans"
+        ["preuntrans"]="preuntrans"
+        ["postuntrans"]="postuntrans"
+        ["verify"]="verifyscript"
+        ["triggerprein"]="triggerprein"
+        ["triggerin"]="triggerin"
+        ["triggerun"]="triggerun"
+        ["triggerpostun"]="triggerpostun"
+        ["filetriggerin"]="filetriggerin"
+        ["filetriggerun"]="filetriggerun"
+        ["transfiletriggerin"]="transfiletriggerin"
+        ["transfiletriggerun"]="transfiletriggerun"
+    )
 
+    # example scripts_content:
+    # postinstall scriptlet (using /bin/sh):
+    # if [ $1 -eq 1 ] && [ -x "/usr/lib/systemd/systemd-update-helper" ]; then
+    #    # Initial installation
+    #    /usr/lib/systemd/systemd-update-helper install-system-units nginx.service || :
+    # fi
     while IFS= read -r line; do
-        if [[ $line =~ ^(postinstall|preuninstall|preinstall|postuninstall|pretrans|posttrans|preupgrade|postupgrade|pre|post)\ scriptlet ]]; then
-            if [ -n "$current_script" ]; then
-                echo "$script_content" > "$output_directory/${script_files[$current_script]}"
-            fi
-            current_script=$(echo $line | awk '{print $1}')
+        if [[ $line =~ ^([a-z]+)\ scriptlet ]]; then
+            generate_to_file "$current_script" "$script_content"
+            current_script=${line%% *}
+            scripts_record+=($current_script)
             script_content=""
         else
             script_content+="$line"$'\n'
         fi
-    done <<< "$scripts_content"
-
-    if [ -n "$current_script" ]; then
-        echo "$script_content" > "$output_directory/${script_files[$current_script]}"
-    fi
+    done <<< "$(rpm -qp --nosignature --scripts "$rpm_package")"
+    generate_to_file "$current_script" "$script_content"
 }
 
-get_phase_content
+extract_install_scripts
 echo "Scripts have been extracted to individual files in $output_directory."
+echo "$rpm_package has scripts: ${scripts_record[@]}"
 echo "========runtimePhase.sh has been generated========="
