680d59ae9fed42f1d5ae93753976465becd9bb01
Yingjiahui <ying_register@163.com>
2025-01-11 16:06:54 +0800

    improve load_enabled_channel_conf: 1. Merge jq operations 2. avoid redundant variable concatenation within the loop.

--- lib/epkg/query.sh
+++ lib/epkg/query.sh
@@ -15,21 +15,21 @@ declare -A channel_array
 
 
 load_enabled_channel_conf() {
-    echo "$CURRENT_PROFILE_DIR/etc/epkg/channel.json"
-    CHANNEL_CONF_PATH="$CURRENT_PROFILE_DIR/etc/epkg/channel.json"
-    json=$(cat $CHANNEL_CONF_PATH)
-    enabled_data=$(echo "$json" | jq -c '[.. | objects | select(.enabled == "1")]')
-    arch=$(uname -m)
+    local CHANNEL_CONF_PATH="$CURRENT_PROFILE_DIR/etc/epkg/channel.json"
+    local arch=$(uname -m)
+    local enabled_data=$(jq -c '[.. | objects | select(.enabled == "1")] | map({key, value: .value})' "$CHANNEL_CONF_PATH")
 
     while IFS= read -r item; do
         key=$(echo "$item" | jq -r '.key')
-        name=$(echo "$item" | jq -r '.value.name')
-        channel=$(echo "$item" | jq -r '.value.channel')
-        url=$(echo "$item" | jq -r '.value.url')${arch}/
-        gpgcheck=$(echo "$item" | jq -r '.value.gpgcheck')
-        gpgkey=$(echo "$item" | jq -r '.value.gpgkey')
+        value=$(echo "$item" | jq -c '.value')
+        # channel.json value
+        name=$(echo "$value" | jq -r '.name')
+        channel=$(echo "$value" | jq -r '.channel')
+        url=$(echo "$value" | jq -r '.url')${arch}/
+        gpgcheck=$(echo "$value" | jq -r '.gpgcheck')
+        gpgkey=$(echo "$value" | jq -r '.gpgkey')
         channel_array[$key]="$name,$channel,$url,$gpgcheck,$gpgkey"
-    done < <(echo "$enabled_data" | jq -c 'to_entries[]')
+    done < <(echo "$enabled_data" | jq -c '.[]')
 }
 
 find_pkg_metadata_json() {
