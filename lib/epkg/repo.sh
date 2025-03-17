e2e4d5735de864ef88be36984c133baceb8ba660
Yingjiahui <ying_register@163.com>
2025-03-17 11:43:18 +0800

    remove dead code in repo.sh

--- lib/epkg/repo.sh
+++ lib/epkg/repo.sh
@@ -15,61 +15,7 @@ init_channel_repo()
 	# installed-packages.json
 	echo -e "{\n}" > $HOME/.epkg/envs/$env/profile-current/installed-packages.json
 
-	# remove in furture: channel.json 
-	local env_channel_json=${HOME}/.epkg/envs/${env}/profile-current/etc/epkg/channel.json
-	local tmp_env_channel_json=/tmp/channel.json
-
-	[[ -d $(dirname ${env_channel_json}) ]] || mkdir -p $(dirname ${env_channel_json})
-	[[ -f ${env_channel_json} ]] || {
-		touch ${env_channel_json}
-		echo -e "{\n}" > ${env_channel_json}
-	}
-
-	local has_channel
-	local channel_content
-	local repo_content
-
-	# /etc/epkg/channel.json:
-	#	channel config file from yum install
-	#
-	# $COMMON_PROFILE_LINK/etc/epkg/channel.json:
-	#	channel config file from script install
-	for channel_json in /etc/epkg/channel.json $COMMON_PROFILE_LINK/etc/epkg/channel.json
-	do
-		[[ -f ${channel_json} ]] || continue
-
-		has_channel=$(cat ${channel_json} | $COMMON_PROFILE_LINK/bin/jq 'has("'"${channel}"'")')
-
-		[[ $has_channel == true ]] || continue
-
-		channel_content=$(cat ${channel_json} | $COMMON_PROFILE_LINK/bin/jq '.["'"${channel}"'"]')
-
-		[[ -z $repo ]] && {
-			$COMMON_PROFILE_LINK/bin/jq --argjson channel_content "${channel_content}" '.["'"${channel}"'"] = '"${channel_content}"'' "${env_channel_json}" > ${tmp_env_channel_json} && \
-				mv -f ${tmp_env_channel_json} ${env_channel_json}
-		}
-
-		[[ -n ${repo} ]] && {
-			repo_content=$(echo "${channel_content}" | $COMMON_PROFILE_LINK/bin/jq '.["'"${repo}"'"]')
-
-			$COMMON_PROFILE_LINK/bin/jq --argjson repo_content "${repo_content}" '.["'"${channel}"'"]["'"${repo}"'"] = '"${repo_content}"'' "${env_channel_json}" > ${tmp_env_channel_json} && \
-				mv -f ${tmp_env_channel_json} ${env_channel_json}
-		}
-	done
-
 	return 0
-}
-
-init_repo_conf()
-{
-	local env=$1
-	local channel_repo=$2
-
-	local channel
-	local repo
-	read channel repo <<< ${channel_repo//\// }
-
-	init_channel_repo ${env} ${channel} ${repo}
 }
 
 list_repos()
