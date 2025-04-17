25b240fb2ab4353182292eebcc6e682d55b2c109
Wu Fengguang <wfg@mail.ustc.edu.cn>
2025-04-17 14:42:57 +0800

    x2epkg: fixup and improve generate_mtree_files()
    Signed-off-by: Wu Fengguang <wfg@mail.ustc.edu.cn>

--- x2epkg/lib/common.sh
+++ x2epkg/lib/common.sh
@@ -4,46 +4,72 @@ epkg_conversion_dir="${HOME}/epkg_conversion"
 
 init_conversion_dirs()
 {
-rm -rf ${epkg_conversion_dir}/*
+    rm -rf ${epkg_conversion_dir}/*
 
-mkdir -p ${epkg_conversion_dir}/{fs,info}
-mkdir -p ${epkg_conversion_dir}/info/pgp
-mkdir -p ${epkg_conversion_dir}/info/install
-touch ${epkg_conversion_dir}/info/{package.json,files}
+    mkdir -p ${epkg_conversion_dir}/{fs,info}
+    mkdir -p ${epkg_conversion_dir}/info/pgp
+    mkdir -p ${epkg_conversion_dir}/info/install
+    touch ${epkg_conversion_dir}/info/{package.json,files}
 }
 
 generate_mtree_files()
 {
-  find "${epkg_conversion_dir}/fs/" -exec stat -c "%n %a %U %G %F" {} + 2>/dev/null | while read -r path mode uname gname type; do
-    relative_path="/${path#${epkg_conversion_dir}/fs/}"
+    # Clear the output file
+    : > "${epkg_conversion_dir}/info/files"
 
-    case "$type" in
-      "regular file")
-        file_type="file"
-        [[ "$mode" != "644" ]] || attributes+="mode=$mode"
-        sha256=$(sha256sum "$path" 2>/dev/null | awk '{print $1}')
-        ;;
-      "directory")
-        file_type="dir"
-        [[ "$mode" != "755" ]] || attributes+="mode=$mode"
-        sha256=""
-        ;;
-      "symbolic link")
-        file_type="link"
-        [[ "$mode" != "777" ]] || attributes+="mode=$mode"
-        sha256=$(sha256sum "$path" 2>/dev/null | awk '{print $1}')
-        ;;
-      *)
-        file_type="$type"
-        sha256=""
-        ;;
-    esac
+    find "${epkg_conversion_dir}/fs/" -exec stat -c "%n %a %U %G %F" {} + 2>/dev/null |
+        while read -r path mode uname gname type
+    do
+        # Get relative path safely
+        local relative_path="${path#${epkg_conversion_dir}/fs/}"
+        [ -z "$relative_path" ] && continue
 
-    [ "$uname" != "root" ] && attributes+=" uname=$uname"
-    [ "$gname" != "root" ] && attributes+=" group=$gname"
-    attributes="type=$file_type"
-    [ -n "$sha256" ] && attributes+=" sha256=$sha256"
+        # Initialize attributes for each file
+        local attributes=
+        local file_type=
+        local sha256=
 
-    echo "$relative_path $attributes" >> "${epkg_conversion_dir}/info/files"
-  done
+        case "$type" in
+            "regular file"|"regular empty file")
+                file_type="file"
+                [ "$mode" = "644" ] && mode=
+                sha256=$(sha256sum "$path")
+                sha256=${sha256%% *}
+                ;;
+            "directory")
+                file_type="dir"
+                [ "$mode" = "755" ] && mode=
+                ;;
+            "symbolic link")
+                file_type="link"
+                [ "$mode" = "777" ] && mode=
+                ;;
+            "character special file")
+                file_type="char"
+                ;;
+            "block special file")
+                file_type="block"
+                ;;
+            "socket")
+                file_type="socket"
+                ;;
+            "fifo")
+                file_type="fifo"
+                ;;
+            *)
+                echo "unknown file type: $type $path"
+                exit 1
+                ;;
+        esac
+
+        # Build attributes string
+        local attrs="type=$file_type"
+        [ -n "$mode" ]          && attrs="$attrs mode=$mode"
+        [ "$uname" != "root" ]  && attrs="$attrs uname=$uname"
+        [ "$gname" != "root" ]  && attrs="$attrs gname=$gname"
+        [ -n "$sha256" ]        && attrs="$attrs sha256=$sha256"
+
+        # Write to file (using printf for reliability with special characters)
+        printf "%s %s\n" "$relative_path" "$attrs" >> "${epkg_conversion_dir}/info/files"
+    done
 }
