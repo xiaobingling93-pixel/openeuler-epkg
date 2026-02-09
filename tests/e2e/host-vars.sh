65079f12d7f17656d2aca3ab57244273eac13121
Wu Fengguang <wfg@mail.ustc.edu.cn>
2026-02-09 11:32:48 +0800

    tests/e2e: use DOCKER_IMAGE=debian to run conda
    Fixes: tests/e2e/test.sh -dd install-remove-upgrade/test-install-remove-upgrade.sh conda bash
       0: Dependency resolution failed for 2nd pass (REQUIRES only):
          bash bash cannot be installed because there are no viable options:
          └─ bash 5.3-h240162b_0 would require
             └─ __glibc __glibc(>=2.17,<3.0.a0), for which no candidates were found.
    
    Signed-off-by: Wu Fengguang <wfg@mail.ustc.edu.cn>

--- tests/e2e/host-vars.sh
+++ tests/e2e/host-vars.sh
@@ -25,5 +25,6 @@ PERSISTENT_OPT_EPKG="/opt/epkg"
 [ -z "$LIGHT_TEST" ] && LIGHT_TEST=1
 
 # Docker configuration
-DOCKER_IMAGE=alpine
+DOCKER_IMAGE=alpine  # Cannot run conda, which relies on host __glibc
+DOCKER_IMAGE=debian
 BUSYBOX_DOCKER=busybox
