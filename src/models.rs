cba917023c216cd3ec7310d98e74d103bfaf7c0e
Wu Fengguang <wfg@mail.ustc.edu.cn>
2026-04-13 10:14:20 +0800

    set DEFAULT_CHANNEL=conda on macos/windows
    On macos, the default DEFAULT_CHANNEL=brew may conflict with upstream
    homebrew:
    
    Error:
       0: HOMEBREW_PREFIX /opt/homebrew already exists and is not empty. On
          macOS, brew environments must use HOMEBREW_PREFIX as env_root.
          Please remove or empty the directory first.
    
    Update windows DEFAULT_CHANNEL for consistency.
    
    Signed-off-by: Wu Fengguang <wfg@mail.ustc.edu.cn>

--- src/models.rs
+++ src/models.rs
@@ -31,9 +31,9 @@ use color_eyre::eyre::WrapErr;
 #[cfg(target_os = "linux")]
 pub const DEFAULT_CHANNEL: &str = &"alpine";
 #[cfg(target_os = "macos")]
-pub const DEFAULT_CHANNEL: &str = &"brew";
+pub const DEFAULT_CHANNEL: &str = &"conda";
 #[cfg(target_os = "windows")]
-pub const DEFAULT_CHANNEL: &str = &"msys2";
+pub const DEFAULT_CHANNEL: &str = &"conda";
 
 pub const SUPPORT_ARCH_LIST: &[&str] = &["aarch64", "x86_64", "riscv64", "loongarch64"];
 pub const PURE_ENV_SUFFIX: char = '!';
